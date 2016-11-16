6de3de715a8d56d84dbeaba8b7be3791621d1492
Francesc Busquets <francesc@gmail.com>
2016-11-16 12:07:11 +0100

    Changed main package to edu.xtec.jclic

--- html5lib/src/edu/xtec/jclic/HTML5LibEditor.java
+++ html5lib/src/edu/xtec/jclic/HTML5LibEditor.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.xtec;
+package edu.xtec.jclic;
 
 import java.io.File;
 import javax.swing.JFileChooser;
