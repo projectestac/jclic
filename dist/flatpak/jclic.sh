58f23fa65ea38ddfd1119701d7036efdc3d3faf1
Ismael Olea <ismael@olea.org>
2020-04-15 12:27:50 +0200

    Moving and cleaning modifications created for the flatpak manifest: they deserve to better be in the relevant linux places.

--- dist/flatpak/jclic.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# jclicplayer wrapper script
-
-# Launch JClic Player
-java -jar /app/JClic/jclic.jar "$@"
-
--- dist/flatpak/jclicauthor.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# jclicauthor wrapper script
-
-# Launch JClic Author
-java -jar /app/JClic/jclicauthor.jar "$@"
